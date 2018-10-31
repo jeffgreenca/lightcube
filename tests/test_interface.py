6d4d05b94506c3d14ef92339ea13f28466cb2d26
Jeff Green <jeff.green@hpe.com>
2018-10-31 14:59:10 -0700

    black

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,13 +1,13 @@
 import unittest
 import interface
 
+
 class TestCubeMock(unittest.TestCase):
-  def setUp(self):
-    self.cube = interface.CubeMock()
+    def setUp(self):
+        self.cube = interface.CubeMock()
 
-  def test_invalid_output(self):
-    self.assertFalse(self.cube.write("?"))
+    def test_invalid_output(self):
+        self.assertFalse(self.cube.write("?"))
 
-  def test_valid_output(self):
-    self.assertTrue(self.cube.write("1"))
-    
+    def test_valid_output(self):
+        self.assertTrue(self.cube.write("1"))
